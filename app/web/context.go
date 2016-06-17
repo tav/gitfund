46b5546a9d6911d24118ef0cf5c22774737dfe9a
tav <tav@espians.com>
2016-06-17 12:48:32 +0100

    app/web: start fleshing out the Context API and dispatcher.

--- app/web/context.go
+++ app/web/context.go
@@ -5,40 +5,138 @@ package web
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"mime"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
 
-	"github.com/tav/gitfund/app/model"
-	"github.com/tav/gitfund/app/page"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
 
-var (
-	DevServer    = false
-	PageTemplate Renderer
-	SiteTemplate Renderer
-)
+var cookieExpire, _ = time.Parse(http.TimeFormat, "Fri, 31 Dec 99 23:59:59 GMT")
 
 type Context struct {
 	AppEngine context.Context
 	Args      []string
-	Buffer    *bytes.Buffer
 	Data      map[string]string
 	Options   map[string]bool
 	Request   *http.Request
-	Response  http.ResponseWriter
-	useBuf    bool
-	user      *model.User
+	buffer    *bytes.Buffer
+	cookies   []string
+	headers   map[string]string
+	output    []byte
+	parsed    bool
+	response  http.ResponseWriter
+	status    int
+	user      interface{}
 	userID    int64
 	xsrf      string
+	values    url.Values
 	written   bool
 }
 
-type Renderer func(c *Context, content []byte)
+func (c *Context) ClearHeaders() {
+	c.headers = map[string]string{}
+}
+
+func (c *Context) DirectOutput(o []byte) {
+	c.output = o
+	c.written = true
+}
+
+func (c *Context) EncodeJSON(v interface{}) {
+	enc := json.NewEncoder(c.buffer)
+	err := enc.Encode(v)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func (c *Context) Errorf(format string, args ...interface{}) {
 	log.Errorf(c.AppEngine, format, args...)
+}
+
+func (c *Context) ExpireCookie(name string) {
+	cookie := &http.Cookie{
+		Name:     name,
+		Expires:  cookieExpire,
+		HttpOnly: true,
+		MaxAge:   -1,
+	}
+	if !DevServer {
+		cookie.Secure = true
+	}
+	c.cookies = append(c.cookies, cookie.String())
+}
+
+func (c *Context) DecodeJSON(v interface{}) error {
+	ct, _, _ := mime.ParseMediaType(c.Request.Header.Get("Content-Type"))
+	if ct != "application/json" {
+		return fmt.Errorf("web: unsupported content type for decoding JSON: %s", ct)
+	}
+	return json.NewDecoder(c.Request.Body).Decode(v)
+}
+
+func (c *Context) parse() {
+	if c.parsed {
+		return
+	}
+	c.values = url.Values{}
+}
+
+func (c *Context) GetBool(attr string) bool {
+	c.parse()
+	if c.values.Get(attr) == "" {
+		return false
+	}
+	return true
+}
+
+func (c *Context) GetCookie(name string) string {
+	cookie, err := c.Request.Cookie(name)
+	if err != nil {
+		return ""
+	}
+	return ParseToken("cookie/"+name, cookie.Value)
+}
+
+func (c *Context) GetInt(attr string) int64 {
+	c.parse()
+	val := c.values.Get(attr)
+	if val == "" {
+		return 0
+	}
+	v, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return v
+}
+
+func (c *Context) GetFile(attr string) bool {
+	return false
+}
+
+func (c *Context) GetString(attr string) string {
+	c.parse()
+	return c.values.Get(attr)
+}
+
+func (c *Context) GetStringSlice(attr string) []string {
+	c.parse()
+	if v, exists := c.values[attr+"[]"]; exists {
+		return v
+	}
+	return []string{}
 }
 
 func (c *Context) Infof(format string, args ...interface{}) {
@@ -53,104 +151,141 @@ func (c *Context) IsTaskRequest() bool {
 	return c.Request.Header.Get("X-AppEngine-TaskName") != "" || DevServer
 }
 
-func (c *Context) Redirect(url string) {
-	if c.written {
-		c.Errorf("web: Redirect called despite response having been written")
-		return
-	}
-	http.Redirect(c.Response, c.Request, url, http.StatusMovedPermanently)
-	c.written = true
+func (c *Context) RaiseNotFound() {
+	panic(raise404{})
 }
 
-func (c *Context) Render(content []byte, renderers ...Renderer) {
-	if len(renderers) == 1 {
-		renderers[0](c, content)
-	} else if len(renderers) == 0 {
-		c.Response.Write(content)
-		c.written = true
-	} else {
-		c.UseBuffer(true)
-		for _, tmpl := range renderers {
-			c.Buffer.Reset()
-			tmpl(c, content)
-			content = c.Buffer.Bytes()
-		}
-		c.UseBuffer(false)
-		c.Response.Write(content)
-		c.written = true
+func (c *Context) RaiseRedirect(url string) {
+	panic(raise301{url})
+}
+
+func (c *Context) Render(renderers ...Renderer) {
+	content := c.buffer.Bytes()
+	for _, renderer := range renderers {
+		c.buffer = &bytes.Buffer{}
+		renderer(c, content)
+		content = c.buffer.Bytes()
 	}
+	c.buffer = &bytes.Buffer{}
+	c.buffer.Write(content)
+}
+
+func (c *Context) RenderContent(content []byte, renderers ...Renderer) {
+	for _, renderer := range renderers {
+		renderer(c, content)
+		content = c.buffer.Bytes()
+		c.buffer = &bytes.Buffer{}
+	}
+	c.buffer.Write(content)
 }
 
 func (c *Context) RenderPage(content []byte) {
-	c.Render(content, PageTemplate, SiteTemplate)
+	c.RenderContent(content, pageRenderers...)
 }
 
-func (c *Context) Serve404() {
-	if c.written {
-		c.Errorf("web: Serve404 called despite response having been written")
-		return
+func (c *Context) SendEmail(from string, to string, body []string) {
+}
+
+func (c *Context) SetCookie(name string, value string) {
+	token := NewToken("cookie/"+name, value, cookieDuration)
+	cookie := &http.Cookie{
+		Name:     name,
+		Value:    token.String(),
+		HttpOnly: true,
+		MaxAge:   cookieSeconds,
 	}
-	c.Data["title"] = "Page Not Found"
-	c.Response.WriteHeader(404)
-	c.Response.Header().Set("Content-Type", "text/html; charset=utf-8")
-	c.RenderPage(page.ERROR_404)
-}
-
-func (c *Context) Serve500() {
-	if c.written {
-		c.Errorf("web: Serve500 called despite response having been written")
-		return
+	if !DevServer {
+		cookie.Secure = true
 	}
-	c.Data["title"] = "Service Unavailable"
-	c.Response.WriteHeader(500)
-	c.Response.Header().Set("Content-Type", "text/html; charset=utf-8")
-	c.RenderPage(page.ERROR_500)
+	c.cookies = append(c.cookies, cookie.String())
 }
 
-func (c *Context) UseBuffer(state bool) {
-	c.useBuf = state
+func (c *Context) SetHeader(name string, value string) {
+	c.headers[name] = value
 }
 
-func (c *Context) User() *model.User {
-	userID := c.UserID()
-	if userID == 0 {
-		return nil
-	}
-	return nil
+func (c *Context) SetStatus(status int) {
+	c.status = status
 }
 
-func (c *Context) UserID() int64 {
-	return 0
+func (c *Context) ValidateXSRF() bool {
+	return c.ValidateXSRFValue(c.GetString("xsrf"))
 }
 
-func (c *Context) ValidateXSRF(token string) bool {
-	return false
+func (c *Context) ValidateXSRFValue(token string) bool {
+	return hmac.Equal([]byte(c.XSRF()), []byte(token))
 }
 
 func (c *Context) Write(p []byte) (int, error) {
-	if c.useBuf {
-		return c.Buffer.Write(p)
-	}
-	c.written = true
-	return c.Response.Write(p)
+	return c.buffer.Write(p)
+}
+
+func (c *Context) WriteString(s string) (int, error) {
+	return c.buffer.Write([]byte(s))
 }
 
 func (c *Context) XSRF() string {
-	return ""
+	if c.xsrf != "" {
+		return c.xsrf
+	}
+	xsrf := c.GetCookie("xsrf")
+	if xsrf != "" {
+		c.xsrf = hex.EncodeToString([]byte(xsrf))
+		return c.xsrf
+	}
+	buf := make([]byte, 36)
+	_, err := rand.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	c.SetCookie("xsrf", string(buf))
+	c.xsrf = hex.EncodeToString(buf)
+	return c.xsrf
+}
+
+func (c *Context) URL(elems ...string) string {
+	args := []string{}
+	kwargs := url.Values{}
+	key := ""
+	for _, elem := range elems {
+		if strings.HasSuffix(elem, "=") {
+			key = elem[:len(elem)-1]
+		} else if key != "" {
+			kwargs.Set(key, elem)
+			key = ""
+		} else {
+			args = append(args, elem)
+		}
+	}
+	scheme := "https"
+	if DevServer {
+		scheme = "http"
+	}
+	host := canonicalHost
+	if host != "" {
+		host = c.Request.Host
+	}
+	u := &url.URL{
+		Scheme:   scheme,
+		Host:     host,
+		Path:     strings.Join(args, "/"),
+		RawQuery: kwargs.Encode(),
+	}
+	return u.String()
 }
 
 func NewContext(w http.ResponseWriter, r *http.Request) *Context {
+	r.Body = http.MaxBytesReader(w, r.Body, 32<<20) // 32MB
 	return &Context{
 		AppEngine: appengine.NewContext(r),
 		Args:      []string{},
-		Buffer:    &bytes.Buffer{},
 		Data:      map[string]string{},
 		Options:   map[string]bool{},
 		Request:   r,
-		Response:  w,
+		buffer:    &bytes.Buffer{},
+		cookies:   []string{},
+		headers:   map[string]string{},
+		response:  w,
+		status:    200,
 	}
-}
-
-func init() {
-	DevServer = appengine.IsDevAppServer()
 }
--- app/web/web.go
+++ app/web/web.go
@@ -4,57 +4,186 @@
 package web
 
 import (
+	"crypto/sha512"
+	"hash"
 	"net/http"
+	"runtime/debug"
+	"strconv"
 	"strings"
+	"time"
+
+	"google.golang.org/appengine"
 )
+
+var DevServer = false
 
 var (
-	Routes RouteMap
-	Router func(*Context, RouteMap)
+	canonicalHost  string
+	cookieDuration time.Duration
+	cookieSeconds  int
+	ensureHost     bool
+	pageRenderers  []Renderer
+	router         func(*Context)
+	routes         RouteMap
+	tokenKey       []byte
+	tokenKeyID     int
+	tokenHash      func() hash.Hash
+	tokenKeys      TokenKeys
 )
 
-type RouteMap map[string]*Handler
-
-type Handler struct {
-	Admin  bool
-	Anon   bool
-	Cron   bool
-	Method func(*Context)
-	Task   bool
-	XSRF   bool
+type Config struct {
+	CanonicalHost  string
+	CookieDuration time.Duration
+	PageRenderers  []Renderer
+	Router         func(*Context)
+	Routes         RouteMap
+	TokenKeys      TokenKeys
 }
 
-func (h *Handler) Call(c *Context) {
-	h.Method(c)
+// raise301 can be used as a value to panic in order to interrupt the control
+// flow and raise a 301 Permanent Redirect.
+type raise301 struct {
+	url string
 }
+
+// raise404 can be used as a value to panic in order to interrupt the control
+// flow and raise a 404 Not Found.
+type raise404 struct{}
+
+type Route struct {
+	Admin     bool
+	Anon      bool
+	Cron      bool
+	Handler   func(*Context)
+	Renderers []Renderer
+	Task      bool
+	XSRF      bool
+}
+
+type Renderer func(c *Context, content []byte)
+
+type RouteMap map[string]*Route
 
 func Dispatch(w http.ResponseWriter, r *http.Request) {
 	c := NewContext(w, r)
+	defer func() {
+		if err := recover(); err != nil {
+			c.buffer.Reset()
+			if redir, ok := err.(raise301); ok {
+				c.headers = map[string]string{
+					"Location": redir.url,
+				}
+				c.status = 301
+			} else if _, ok := err.(raise404); ok {
+				c.serve404()
+			} else {
+				c.Errorf("%v\n%s", err, string(debug.Stack()))
+				c.serve500()
+			}
+		}
+		cl := false
+		ct := false
+		hdrs := w.Header()
+		for k, v := range c.headers {
+			switch strings.ToLower(k) {
+			case "content-length":
+				cl = true
+			case "content-type":
+				ct = true
+			}
+			hdrs.Set(k, v)
+		}
+		var out []byte
+		if c.written {
+			out = c.output
+		} else {
+			out = c.buffer.Bytes()
+		}
+		clen := len(out)
+		if !cl {
+			hdrs.Set("Content-Length", strconv.FormatInt(int64(clen), 10))
+		}
+		if !ct {
+			hdrs.Set("Content-Type", "text/html; charset=utf-8")
+		}
+		for _, v := range c.cookies {
+			hdrs.Set("Set-Cookie", v)
+		}
+		w.WriteHeader(c.status)
+		if clen != 0 {
+			w.Write(out)
+		}
+	}()
+	if !DevServer {
+		badURL := false
+		if r.TLS == nil {
+			badURL = true
+		} else if ensureHost && r.Host != canonicalHost && !(c.IsCronRequest() || c.IsTaskRequest()) {
+			badURL = true
+		}
+		if badURL {
+			host := canonicalHost
+			if host == "" {
+				host = r.Host
+			}
+			if r.Method == "GET" || r.Method == "HEAD" {
+				url := r.URL
+				url.Scheme = "https"
+				url.Host = host
+				c.RaiseRedirect(url.String())
+			} else {
+				c.RaiseRedirect("https://" + host + "/")
+			}
+		}
+	}
 	path := r.URL.Path
-	// defer func() {
-	// 	if err := recover(); err != nil {
-	// 		c.Errorf("%v\n%s", err, string(debug.Stack()))
-	// 		c.Serve500()
-	// 	}
-	// }()
 	if !strings.HasPrefix(path, "/") {
-		c.Serve404()
+		c.serve404()
 		return
 	}
 	path = path[1:]
-	if path == "" {
-		// c.Pipe(site.RenderHome(c), site.Page, site.Main)
-		return
-	}
 	elems := strings.Split(path, "/")
-	if handler, ok := Routes[elems[0]]; ok {
+	if path == "" {
+		elems[0] = "/"
+	}
+	if route, ok := routes[elems[0]]; ok {
 		for _, elem := range elems[1:] {
 			if elem != "" {
 				c.Args = append(c.Args, elem)
 			}
 		}
-		handler.Call(c)
+		route.Handler(c)
 		return
 	}
-	c.Serve404()
+	c.serve404()
+}
+
+func Init(c *Config) {
+	DevServer = appengine.IsDevAppServer()
+	canonicalHost = c.CanonicalHost
+	if canonicalHost != "" {
+		ensureHost = true
+	}
+	cookieDuration = c.CookieDuration
+	if cookieDuration == 0 {
+		cookieDuration = 14 * (24 * time.Hour)
+	}
+	cookieSeconds = int(cookieDuration / time.Second)
+	pageRenderers = c.PageRenderers
+	router = c.Router
+	routes = c.Routes
+	tokenKeyID = 0
+	tokenKeys = c.TokenKeys
+	for keyID, spec := range tokenKeys {
+		if keyID > tokenKeyID {
+			tokenKeyID = keyID
+		}
+		if spec.Hash == nil {
+			spec.Hash = sha512.New384
+		}
+	}
+	tokenKey = tokenKeys[tokenKeyID].Key
+	tokenHash = tokenKeys[tokenKeyID].Hash
+	http.HandleFunc("/", Dispatch)
+
 }
